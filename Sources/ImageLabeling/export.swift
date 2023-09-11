66b9431cf6be36b43ef3b27fcfe551f52b22ba2d
Constantin Lungu <constantin@mixbook.com>
2023-09-11 19:53:17 +0300

    Maybe #6

--- Sources/ImageLabeling/export.swift
+++ Sources/ImageLabeling/export.swift
@@ -1,4 +1,33 @@
-@_exported import MLKitCommon
-@_exported import MLKitImageLabelingCommon
-@_exported import MLKitVision
-@_exported import MLKitVisionKit
+import MLKitImageLabeling
+import MLKitVision
+
+public enum Analyzer {
+  public static func mlKitAnalysis(image: UIImage, completion: @escaping ([ImageLabel]?, Error?) -> Void) {
+    let visionImage = VisionImage(image: image)
+    visionImage.orientation = image.imageOrientation
+
+    let options = ImageLabelerOptions()
+    options.confidenceThreshold = NSNumber(floatLiteral: 0.3)
+
+    let imageLabeler = ImageLabeler.imageLabeler(options: options)
+
+    imageLabeler.process(visionImage) { (labels: [ImageLabel]?, error: Error?) in
+      guard error == nil else {
+        let errStr = error?.localizedDescription ?? ""
+        print("ML Kit error: \(errStr)")
+        completion(nil, error)
+        return
+      }
+
+      guard let labels, !labels.isEmpty else {
+        print("ML Kit error: no labels found")
+        let error = NSError(domain: "Testbed ML Kit: no labels", code: -1)
+        completion(nil, error)
+        return
+      }
+
+      completion(labels, nil)
+    }
+  }
+}
+
